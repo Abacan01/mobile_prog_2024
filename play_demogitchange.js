b6a34a45356746c7fcdd6ea5cea3e1bd0697341d
Ian <2200983@ub.edu.ph>
2024-09-07 10:59:50 +0800

    HAYOP KA

---
+++ play_demogitchange.js
@@ -0,0 +1,20 @@
+console.log("Demot GIT change");
+
+//adding new projet and using git
+//1. create a new repository on github 
+//2. Go to folder where your project is located
+//3. run git init 
+//4. rin git add .
+//5. run git commit -m "inital commit"
+//6. run git branch -main
+//7. Run git reote add prigin <url>
+//8. run git push -u origin main
+
+
+// Add/edit, commit and push 
+/*
+
+1.git add .
+2.git commit -, "Demo GIT.Change"
+3.git push
+*/
